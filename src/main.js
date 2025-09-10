ae261058d1a7c59be354cbd527a90dce30c7f605
Shakir Salam <shakirsalam555@Gmail.com>
2025-09-10 21:57:13 +0800

    init tailwindcss framework

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./style.css";   // ⬅️ important
+import App from "./counter.js";
+
+document.querySelector("#app").innerHTML = App();
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // scan all source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
