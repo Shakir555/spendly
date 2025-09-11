56a7803b955d758973ecabb07166237dc7c97d63
Shakir Salam <shakirsalam555@Gmail.com>
2025-09-11 22:17:37 +0800

    update addExpense.js

--- src/components/addExpense.js
+++ src/components/addExpense.js
@@ -1,24 +1,24 @@
 // components/addexpense.js
 export default function AddExpenseForm() {
   return `
-    <div class="border rounded-lg p-4 mb-6 shadow bg-white">
+    <div class="border rounded-lg p-4 mb-6 shadow bg-blue">
       <h3 class="text-lg font-bold mb-3">Add Expense</h3>
       <form class="flex flex-col gap-3">
-        <input 
-          type="text" 
-          placeholder="Expense name" 
-          class="border rounded-md p-2"
-        />
-        <input 
-          type="number" 
-          placeholder="Amount" 
-          class="border rounded-md p-2"
-        />
+        <!-- Category -->
         <select class="border rounded-md p-2">
-          <option>Food</option>
-          <option>Transport</option>
-          <option>Shopping</option>
-        </select>
+            <option>Food</option>
+            <option>Transport</option>
+            <option>Shopping</option>
+            <option>Groceries</option>
+        </select> 
+        <!-- Amount -->
+        <input type="number" placeholder="$$$" class="border rounded-md p-2"/>
+        <!-- Date -->
+        <input type="datetime-local" class="border rounded-md p-2"/>
+        <!-- Notes -->
+        <textarea placeholder="Add Notes.." class="border rounded-md p-2 resize-y min-h-[80px]">
+        </textarea>
+        <!-- Add Expense Button>
         <button type="submit" class="bg-blue-500 text-white p-2 rounded-md">
           Add Expense
         </button>
