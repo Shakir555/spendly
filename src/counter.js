d95d7011719a59457bcda24372121493d1d7b3e9
Shakir Salam <shakirsalam555@Gmail.com>
2025-09-11 21:42:16 +0800

    main components init: total, avg/day, top category, budget left

--- src/counter.js
+++
@@ -1,7 +0,0 @@
-// export default function App() {
-//   return `
-//     <div class="p-10 bg-gray-100">
-//       <h1 class="text-4xl font-bold text-red-500">Hello Tailwind!</h1>
-//     </div>
-//   `;
-// }
--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ document.querySelector("#app").innerHTML = `
   <div class="p-6 min-h-screen">
     <!-- App Title -->
     <h1 class="text-3xl font-bold text-center mb-6">Spendly</h1>
-
-    <!-- Header Row -->
+    <!-- Header -->
     <div class="flex justify-between items-center mb-6">
       <h2 class="text-2xl font-bold">Monthly Expenses</h2>
       <select class="border rounded-md p-2 font-bold">
@@ -13,5 +12,31 @@ document.querySelector("#app").innerHTML = `
         <option>Aug 2025</option>
       </select>
     </div>
-  </div>
+
+    <!-- Components -->
+    <div class="flex justify-between mb-3">
+      <!-- Total -->
+      <div class="flex flex-col item-center">
+        <h4 class="text-lg font-bold">Total</h4>
+        <p class="text-xl font-thin">$$$$</p>
+      </div>
+      <!-- Avg/Day -->
+      <div class="flex flex-col items-center">
+        <h4 class="text-lg font-bold">Avg/Day</h4>
+        <p class="text-xl font-thin">$$$$</p>
+      </div>
+      <!-- Top Category -->
+      <div class="flex flex-col items-center">
+        <h4 class="text-lg font-bold">Top Category</h4>
+        <p class="text-xl font-thin">$$$$</p>
+      </div>
+      <!-- Remaining Budget -->
+      <div class="flex flex-col items-center">
+        <h4 class="text-lg font-bold">Remaining Budget</h4>
+        <p class="text-xl font-thin">$$$$</p> 
+      </div>
+    </div>       
+  </div>  
 `;
+
+
