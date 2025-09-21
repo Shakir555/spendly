bc02dfffaa37826d88f5d9ea78cf526cc1cec689
Shakir Salam <shakirsalam555@Gmail.com>
2025-09-21 11:42:56 +0800

    update monthly expenses ui

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import { setupAddExpenseEvent } from "./events/addExpenseEvent.js";
 document.querySelector("#app").innerHTML = `
   <div class="p-6 min-h-screen">
     <!-- App Title -->
-    <h1 class="text-3xl font-bold text-center mb-6">Spendly</h1>
+    <h1 class="text-3xl font-extrabold text-center mb-6 drop-shadow-lg">Spendly</h1>
     <!-- Header -->
-    <div class="flex justify-between items-center mb-6">
+    <div class="flex justify-between items-center mb-6 border-2 border-blue-500 rounded-lg p-4">
       <h2 class="text-2xl font-bold">Monthly Expenses</h2>
       <select class="border rounded-md p-2 font-bold">
         <option>Sept 2025</option>
@@ -19,26 +19,29 @@ document.querySelector("#app").innerHTML = `
     </div>
 
     <!-- Components -->
-    <div class="flex justify-between mb-3">
+    <div class="flex justify-around items-center mb-3 border-2 border-blue-500 rounded-lg p-4 bg-white shadow-sm">
       <!-- Total -->
-      <div class="flex flex-col item-center">
+      <div class="flex flex-col items-center mx-2">
         <h4 class="text-lg font-bold">Total</h4>
         <p class="text-xl font-thin">$$$$</p>
       </div>
+
       <!-- Avg/Day -->
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col items-center mx-2">
         <h4 class="text-lg font-bold">Avg/Day</h4>
         <p class="text-xl font-thin">$$$$</p>
       </div>
+
       <!-- Top Category -->
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col items-center mx-2">
         <h4 class="text-lg font-bold">Top Category</h4>
         <p class="text-xl font-thin">$$$$</p>
       </div>
+
       <!-- Remaining Budget -->
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col items-center mx-2">
         <h4 class="text-lg font-bold">Remaining Budget</h4>
-        <p class="text-xl font-thin">$$$$</p> 
+        <p class="text-xl font-thin">$$$$</p>
       </div>
     </div>
 
